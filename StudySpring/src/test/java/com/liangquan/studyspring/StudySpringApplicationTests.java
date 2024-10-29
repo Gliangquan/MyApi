4735411380cf4340b003ac19ae12251663e84020
liangquan <liangquan0000@outlook.com>
2024-10-29 19:37:25 +0800

    1

---
+++ StudySpring/src/test/java/com/liangquan/studyspring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangquan.studyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
